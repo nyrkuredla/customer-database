d26db69065f6669376c1e487f2fcd1cbb30677d7
Ryan Kelley <rtkelley@gmail.com>
2017-08-01 17:46:15 -0400

    cleaned up JS commented out fluff

--- main.js
+++ main.js
@@ -2,23 +2,6 @@
 const custInfoWrapper = document.getElementById("customer-info-wrapper");
 
 
-// // creating new div to insert template into for each cust
-// function createCustDiv () {
-//   let custInfoDiv = document.createElement("div");
-//   custInfoDiv.classList.add("customer-info");
-//   custInfoWrapper.appendChild(custInfoDiv);
-//   custInfoDiv.innerHTML = custInfoTemp;
-// }
-
-// creating variables to populate customer info template literal
-let custPrefix = customers.results[0];
-
-
-// // function to add custInfoTemp to customer info div
-// function insertCustTemp () {
-//   custInfoDiv.innerHTML = custInfoTemp;
-// }
-
 // creating new template literals and populating with custInfo from customer data array
 for (let i = 0; i < customers.results.length; i++) {
   custPrefix = customers.results[i];
@@ -36,8 +19,9 @@ for (let i = 0; i < customers.results.length; i++) {
   // creating template literal
   const custInfoTemp = `
     <img src=${image} />
-    <p class="name">${nameFirst} ${nameLast}</p>
-    <p class="email">${email}</p>
+    <div class="cust-name-email">
+      <p class="name">${nameFirst} ${nameLast}</p>
+      <p class="email">${email}</p>
     </div>
     <div class="cust-loc">
       <p class="locStreet">${locStreet}</p>
@@ -53,40 +37,3 @@ for (let i = 0; i < customers.results.length; i++) {
   custInfoDiv.innerHTML = custInfoTemp;
 
 }
-
-
-
-
-
-
-//
-//
-// let customers = data.results;
-// let html = "";
-// for (i = 0; i < customers.length; i++) {
-//   var customer = customers[i];
-//
-//   html += newCustomer(customer.picture.medium, )
-// }
-//
-// document.querySelector(".container").innerHTML = html;
-//
-// function newCustomer(img, first, last, email, street, city, state, postcode, tel, ssn) {
-//   let post =
-//   // paste template here
-//   `<img src=${img} />
-//   <div class="cust-info1">
-//     <p class="nameFirst">${first}</p>
-//     <p class="nameLast">${last}</p>
-//     <p class="email">${email}</p>
-//   </div>
-//   <div class="cust-info2">
-//     <p class="locStreet">${street}</p>
-//     <p class="locGlobal">${city} ${state} ${postcode}</p>
-//   </div>
-//   <div class="cust-info3">
-//     <p class="phone">${tel}</p>
-//     <p class="ssn">${ssn}</p>
-//   </div>`
-//   return post;
-// }
