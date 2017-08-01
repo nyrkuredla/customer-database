9d93a2955964150d1c05284172d3f5e7180f6f86
Ryan Kelley <rtkelley@gmail.com>
2017-08-01 15:30:29 -0400

    finished up template literal, starting loop

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 // defining customer info wrapper to put new cust info divs in
 const custInfoWrapper = document.getElementById("customer-info-wrapper");
 
-// creating array to populate customer info template literal
-let info = customers.results[i];
-let custInfoVars = [];
+// creating variables to populate customer info template literal
+let custPrefix = customers.results[0];
+const custInfoVars = [];
+for (let i = 0; i < custPrefix.length; i++) {
+  custPrefix = customers.results[i];
+  custInfoVars
+}
 
-// setting array to populate customer info temp literal
+const image = custPrefix.picture.medium;
+const nameFirst = custPrefix.name.first;
+const nameLast = custPrefix.name.last;
+const email = custPrefix.email;
+const locStreet = custPrefix.location.street;
+const locCity = custPrefix.location.city;
+const locState = custPrefix.location.state;
+const locPostcode = custPrefix.location.postcode;
+const phone = custPrefix.phone;
+const ssn = custPrefix.id.value;
+
 
 // creating template literal
 const custInfoTemp = `
@@ -17,7 +31,7 @@ const custInfoTemp = `
   </div>
   <div class="cust-info2">
     <p class="locStreet">${locStreet}</p>
-    <p class="locGlobal">${locGlobal}</p>
+    <p class="locGlobal">${locCity} ${locState} ${locPostcode}</p>
   </div>
   <div class="cust-info3">
     <p class="phone">${phone}</p>
